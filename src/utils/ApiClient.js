d07d28d3a3957e400b30a1f5dc819b8682cdc311
raday146 <57037365+raday146@users.noreply.github.com>
2021-11-17 22:33:07 +0200

    first commit

---
+++ src/utils/ApiClient.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+const client = axios.create();
+
+export async function authenticate() {}
+export async function logIn() {}
+export async function signUp() {}
+export async function logOut() {}
+export async function getNotification() {}
+export async function readNotification() {}
